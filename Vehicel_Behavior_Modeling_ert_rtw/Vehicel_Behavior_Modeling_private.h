612b9f1d4ebadaaf39e5b82c72d1b122e62788db
Challa200Santhosh <sschalla10@gmail.com>
2026-07-28 20:51:29 +0530

    bit modification done today
    check the files

---
+++ Vehicel_Behavior_Modeling_ert_rtw/Vehicel_Behavior_Modeling_private.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: Vehicel_Behavior_Modeling_private.h
+ *
+ * Code generated for Simulink model 'Vehicel_Behavior_Modeling'.
+ *
+ * Model version                  : 3.10
+ * Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+ * C/C++ source code generated on : Tue Jul 28 16:21:27 2026
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef Vehicel_Behavior_Modeling_private_h_
+#define Vehicel_Behavior_Modeling_private_h_
+#include "rtwtypes.h"
+#include "Vehicel_Behavior_Modeling_types.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+#endif                                /* Vehicel_Behavior_Modeling_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
